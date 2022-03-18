6945a230f77a6c53be82428a1487730ee81c632c
iamcoolcat <nelit.westley@yandex.com>
2022-03-18 11:34:06 +0100

    try to edit putchar file later

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* main - prints Holberton, followed by a new line.
+*
+* Return: 0 (Success)
+*/
+
+
+int main(void)
+{
+	char *holberton;
+
+	for (holberton = "Holberton\n"; *holberton != '\0'; ++holberton)
+		_putchar(*holberton);
+	return (0);
+}
