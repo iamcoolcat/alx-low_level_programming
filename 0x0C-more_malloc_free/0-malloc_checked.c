c80217899720a824f2198e1d820a4f29dfa7649f
iamcoolcat <77372632+iamcoolcat@users.noreply.github.com>
2022-04-07 22:32:52 +0100

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ *
+ * @b: size of memory
+ *
+ * Return: memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memory;
+
+	memory = malloc(b);
+
+	if (!memory && b)
+	{
+	exit(98);
+	}
+	return (memory);
+}
